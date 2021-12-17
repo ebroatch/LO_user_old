7feea164a98a16b8299387523bf80ed1b72e5de6
Erin <ebroatch@uw.edu>
2021-12-16 17:12:11 -0800

    Update job_lists.py
    add orca job list

--- extract/moor/job_lists.py
+++ extract/moor/job_lists.py
@@ -10,7 +10,7 @@ def get_sta_dict(job_name):
         sta_dict = {
             'wbc': (-123.9516, 46.6290)
             }
-            
+
     elif job_name == 'mickett_1':
         sta_dict = {
             'ORCA_Hansville': (-122.6270, 47.9073),
@@ -19,13 +19,13 @@ def get_sta_dict(job_name):
             'Central_Main_Stem_Hood_Canal': (-122.989507, 47.574352),
             'North_Central_Main_Basin': (-122.440755, 47.825099)
         }
-            
+
     elif job_name == 'mickett_2':
         sta_dict = {
             'Carr_Inlet_ORCA': (-122 - 43.8/60, 47 + 16.8/60),
             'East_of_Fox_Island': (-122 - 35.158/60, 47 + 13.185/60)
         }
-        
+
     elif job_name == 'stoll_corals':
         sta_dict = {
         'Carson_D01_Lopez': (-122.8728, 48.36816),
@@ -39,8 +39,8 @@ def get_sta_dict(job_name):
         'USNM_19228_Dungeness': (-123.189, 48.225),
         'USNM_19272_Admiralty': (-122.817, 48.20),
         'USNM_92620_Lopez': (-122.85, 48.3667),
-        	}
-            
+        }
+
     elif job_name == 'stoll_obs':
         sta_dict = {
         'DOE_SJF002': (-123.025, 48.25),
@@ -49,16 +49,16 @@ def get_sta_dict(job_name):
         'WOAC_STN21': (-122.8504, 48.1883),
         'WOAC_STN20': (-122.6848, 48.142),
         'WOAC_STN19': (-122.6318, 48.0915),
-            }
-            
+        }
+
     elif job_name == 'Kelly':
         # note I pushed two of the locations a bit West to get off the landmask
         sta_dict = {
         'Seal_Rock': (-122.87004, 47.70557),
         'Little_Dewatto': (-123.08612-.005, 47.44489),
         'Red_Bluff': (-123.10438-.007, 47.41625)
-            }
-            
+        }
+
     elif job_name == 'jazzy':
         sta_dict = {
         'Middle_Bank': (-123.09651, 48.40935),
@@ -69,8 +69,18 @@ def get_sta_dict(job_name):
         'North_Station': (-123.04166, 48.58330),
         'South_Station': (-122.94330, 48.42000),
         'Hein_Bank': (-123.03940, 48.35825)
-            }
-            
+        }
+
+    elif job_name == 'orca_eb':
+        sta_dict = {
+        'CI': (-122.7300, 47.2800),
+        'HV': (-122.6270, 47.9073),
+        'DB': (-122.8029, 47.8034),
+        'HP': (-123.1126, 47.4218),
+        'TW': (-123.0083, 47.3750),
+        'PW': (-122.3972, 47.7612)
+        }
+
     else:
         print('Unsupported job name!')
         a = dict()
